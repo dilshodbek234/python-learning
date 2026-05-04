571546ffe730c495f62a50b3e11d8579f554b76a
dilshodbek234 <ziyadullayevdilshodbek5@gmail.com>
2026-05-04 19:40:26 +0500

    os.getlogin xatosi tuzatildi va barqarorlashdi

--- tizim.py
+++ tizim.py
@@ -1,12 +1,16 @@
 import platform
 import os
+import getpass # Yangi kutubxona qo'shdik
 
 def noutbuk_malumoti():
     print("--- HP Noutbuk Tizim Malumotlari ---")
     print(f"Operatsion tizim: {platform.system()} {platform.release()}")
     print(f"Protsessor: {platform.processor()}")
     print(f"Mashina turi: {platform.machine()}")
-    print(f"Foydalanuvchi: {os.getlogin()}")
+    
+    # os.getlogin() o'rniga mana buni ishlatamiz:
+    foydalanuvchi = getpass.getuser()
+    print(f"Foydalanuvchi: {foydalanuvchi}")
     print("-" * 35)
 
 if __name__ == "__main__":
